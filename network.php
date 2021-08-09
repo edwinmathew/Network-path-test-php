ebbac88d87dbac768eaa6d014431fdc2a969a08c
edwin <edwinmathew63@gmail.com>
2021-08-09 19:04:52 +0530

    network.php

--- network.php
+++ network.php
@@ -10,7 +10,7 @@ class Network_search extends Database{
 		
 		
 	function get_csv_file(){
-		echo "Enter csv file name";
+		echo "Enter csv file name". PHP_EOL;;
 		$f = fopen( 'php://stdin', 'r' );
 		$this->line = fgets( $f );
 		fclose( $f );
@@ -40,6 +40,13 @@ class Network_search extends Database{
 		
 	} 
 	
+	function truncate_table()
+	{
+		$sql = "TRUNCATE TABLE paths";
+		
+		$a=mysqli_query(self::$conn,$sql);
+
+	}
 	
 	function insert_csv()
 	{
@@ -52,12 +59,13 @@ class Network_search extends Database{
 	
 	function get_user_input()
 	{
+		
 		$f = fopen( 'php://stdin', 'r' );
 		$line = fgets($f);
-		$this->process_csv($line);
+		$this->find_path($line);
 		while (trim($line)!='QUIT') {
-			$line = fgets( $f );
-			echo $line;
+			$line = fgets($f);
+			$this->find_path($line);
 		}
 		fclose( $f );
 		
@@ -66,11 +74,48 @@ class Network_search extends Database{
 	
 	
 	
+	function find_path($line)
+	{
+		$test_data  	= explode(' ',trim($line));
+		$start_node		= $test_data['0'];
+		$end_node		= $test_data['1'];
+		$maximum_time	= $test_data['2'];
+		$sql = "WITH RECURSIVE cte AS(SELECT p.destination,concat(p.source, '=>', p.destination) 	AS path,signal_time,path_id FROM paths p WHERE p.source = '".$start_node."' UNION ALL SELECT p.destination, concat(c.path, '=>', p.destination) AS path,p.signal_time + c.signal_time,p.path_id+c.path_id FROM cte c JOIN paths p ON p.source = c.destination) SELECT c.path,c.signal_time,c.path_id FROM cte c WHERE c.destination = '".$end_node."' AND c.signal_time <='".$maximum_time."' ORDER BY c.path_id LIMIT 1;";
+	$result=mysqli_query(self::$conn,$sql);
+		if (mysqli_num_rows($result) > 0) {
+		  // output data of each row
+		  while($row = $result->fetch_assoc()) {
+			echo $row['path'].'=>'.$row['signal_time']. PHP_EOL;
+		  }
+		} else {
+		 $this->find_path_reverse();
+		}
+	}
+	
+	function find_path_reverse()
+	{
+		$test_data  	= explode(' ',trim($this->line));
+		$start_node		= $test_data['0'];
+		$end_node		= $test_data['1'];
+		$maximum_time	= $test_data['2'];
+        $sql = "WITH RECURSIVE cte AS(SELECT p.destination,concat(p.destination,'=>',p.source) 	AS path,signal_time,path_id FROM paths p WHERE p.source = '".$end_node."' UNION ALL SELECT p.destination, concat(p.destination,'=>',c.path) AS path,p.signal_time + c.signal_time ,p.path_id+c.path_id FROM cte c JOIN paths p ON p.source = c.destination) SELECT c.path,c.signal_time,c.path_id FROM cte c WHERE c.destination = '".$start_node."' AND c.signal_time <='".$maximum_time."' ORDER BY c.path_id LIMIT 1;";
+		$result=mysqli_query(self::$conn,$sql);
+		if (mysqli_num_rows($result) > 0) {
+		
+		  // output data of each row
+		  while($row = $result->fetch_assoc()) {
+			echo $row['path'].'=>'.$row['signal_time']. PHP_EOL;
+		  }
+		} else {
+		  echo "No path found";
+		}
+	}
 	
 }
 $network = new Network_search();
 $network->get_csv_file();
 $network->process_csv();
+$network->truncate_table();
 $network->insert_csv();
 $network->get_user_input()
 ?>
