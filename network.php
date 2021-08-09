d315dd3b78647c91d168e05a5d2c8e5ba367552d
edwin <edwinmathew63@gmail.com>
2021-08-09 19:25:32 +0530

    Function added to clean user input

--- network.php
+++ network.php
@@ -8,11 +8,13 @@ class Network_search extends Database{
 			parent::__construct();
 		}
 		
-		
+	//To get the csv file name
+	
 	function get_csv_file(){
 		echo "Enter csv file name". PHP_EOL;;
 		$f = fopen( 'php://stdin', 'r' );
 		$this->line = fgets( $f );
+		$this->line =$this->clean_input_data($this->line);
 		fclose( $f );
 	}
 	
@@ -25,11 +27,13 @@ class Network_search extends Database{
 				$row++;
 				$row_array=array();
 				for ($c=0; $c < $num; $c++) {
+					$data[$c] =$this->clean_input_data($data[$c]);
 					if(!is_numeric($data[$c])){
 						$rowValues[$c] ="'".$data[$c]."'";
 					}else{
 						$rowValues[$c] =$data[$c];
 					}
+					
 					
 				}
 				 $this->csv_array[] = "(" . implode(', ', $rowValues) . ")";
@@ -62,9 +66,10 @@ class Network_search extends Database{
 		
 		$f = fopen( 'php://stdin', 'r' );
 		$line = fgets($f);
-		$this->find_path($line);
-		while (trim($line)!='QUIT') {
-			$line = fgets($f);
+		$this->line = $this->clean_input_data($line);
+		$this->find_path();
+		while (trim($this->line)!='QUIT') {
+			$this->line = fgets($f);
 			$this->find_path($line);
 		}
 		fclose( $f );
@@ -74,12 +79,12 @@ class Network_search extends Database{
 	
 	
 	
-	function find_path($line)
+	function find_path()
 	{
-		$test_data  	= explode(' ',trim($line));
-		$start_node		= $test_data['0'];
-		$end_node		= $test_data['1'];
-		$maximum_time	= $test_data['2'];
+		$test_data  	= explode(' ',trim($this->line));
+		$start_node		=  $this->clean_input_data($test_data['0']);
+		$end_node		=  $this->clean_input_data($test_data['1']);
+		$maximum_time	=  $this->clean_input_data($test_data['2']);
 		$sql = "WITH RECURSIVE cte AS(SELECT p.destination,concat(p.source, '=>', p.destination) 	AS path,signal_time,path_id FROM paths p WHERE p.source = '".$start_node."' UNION ALL SELECT p.destination, concat(c.path, '=>', p.destination) AS path,p.signal_time + c.signal_time,p.path_id+c.path_id FROM cte c JOIN paths p ON p.source = c.destination) SELECT c.path,c.signal_time,c.path_id FROM cte c WHERE c.destination = '".$end_node."' AND c.signal_time <='".$maximum_time."' ORDER BY c.path_id LIMIT 1;";
 	$result=mysqli_query(self::$conn,$sql);
 		if (mysqli_num_rows($result) > 0) {
@@ -95,9 +100,9 @@ class Network_search extends Database{
 	function find_path_reverse()
 	{
 		$test_data  	= explode(' ',trim($this->line));
-		$start_node		= $test_data['0'];
-		$end_node		= $test_data['1'];
-		$maximum_time	= $test_data['2'];
+		$start_node		=  $this->clean_input_data($test_data['0']);
+		$end_node		=  $this->clean_input_data($test_data['1']);
+		$maximum_time	=  $this->clean_input_data($test_data['2']);
         $sql = "WITH RECURSIVE cte AS(SELECT p.destination,concat(p.destination,'=>',p.source) 	AS path,signal_time,path_id FROM paths p WHERE p.source = '".$end_node."' UNION ALL SELECT p.destination, concat(p.destination,'=>',c.path) AS path,p.signal_time + c.signal_time ,p.path_id+c.path_id FROM cte c JOIN paths p ON p.source = c.destination) SELECT c.path,c.signal_time,c.path_id FROM cte c WHERE c.destination = '".$start_node."' AND c.signal_time <='".$maximum_time."' ORDER BY c.path_id LIMIT 1;";
 		$result=mysqli_query(self::$conn,$sql);
 		if (mysqli_num_rows($result) > 0) {
@@ -107,8 +112,17 @@ class Network_search extends Database{
 			echo $row['path'].'=>'.$row['signal_time']. PHP_EOL;
 		  }
 		} else {
-		  echo "No path found";
+		  echo "No path found". PHP_EOL;
 		}
+	}
+	
+	function clean_input_data($data)
+	{
+		$data = htmlspecialchars($data);
+		$data = stripslashes($data);
+		$data = trim($data);
+		$data = mysqli_real_escape_string(self::$conn,$data);
+		return $data;
 	}
 	
 }
